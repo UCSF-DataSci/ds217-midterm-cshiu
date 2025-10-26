3e5d0312ba672c114005ba3924083bee4426d8dc
Cori <220386746+cshiu@users.noreply.github.com>
2025-10-26 14:35:31 -0700

    q1 v3: made chmod command executable + updated directory structure

--- q1_setup_project.sh
+++ q1_setup_project.sh
@@ -4,7 +4,7 @@
 # Assignment 5, Question 1: Project Setup Script
 # This script creates the directory structure for the clinical trial analysis project
 # TODO: Make this script executable (if not already)
-# chmod +x q1_setup_project.sh
+chmod +x q1_setup_project.sh
 
 # TODO: Create the following directories:
 #   - data/
